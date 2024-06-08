7a559d72fa7158355b8b9b340fbccd28d6e4cc1e
MizutamariShutarou <shutaro0415@outlook.jp>
2024-06-08 18:08:54 +0900

    [update] Title,InGame,Finishでゲームの状態を区別

--- PA21_MizutamariShutaro_2024T1/Main.cpp
+++ PA21_MizutamariShutaro_2024T1/Main.cpp
@@ -5,19 +5,38 @@ constexpr int BALL_RAD = 40;
 
 constexpr float BALL_SPEED = 300.0;
 
-constexpr Size BRICK_SIZE{ 40, 20 };
+constexpr Size BRICK_SIZE{40, 20};
 
 constexpr int X_COUNT = 20;
 constexpr int Y_COUNT = 5;
 constexpr int MAX = X_COUNT * Y_COUNT;
 
+constexpr float INTERVAL_TIMER = 0;
+float _timer = 0;
+float _isActiveTimer = 0;
+
+bool _isActive = false;
+
+constexpr enum STATE
+{
+	Title,
+	InGame,
+	Finish,
+};
+
+STATE _state;
+
 void Main()
 {
-	Vec2 ballVec{ 0, BALL_SPEED };
+	_state = Title;
 
-	Circle ball{ 400, 400, 8 };
+	Font _text{30};
 
-	Rect bricks[ MAX ];
+	Vec2 ballVec{0, BALL_SPEED};
+
+	Circle ball{400, 400, 8};
+
+	Rect bricks[MAX];
 
 	for (int i = 0; i < X_COUNT; ++i)
 	{
@@ -25,17 +44,86 @@ void Main()
 		{
 			int index = (j * X_COUNT) + i;
 
-			bricks[index] = Rect{ i * BRICK_SIZE.x, 60 + j * BRICK_SIZE.y, BRICK_SIZE };
+			bricks[index] = Rect{i * BRICK_SIZE.x, 60 + j * BRICK_SIZE.y, BRICK_SIZE};
 		}
 	}
 
 	while (System::Update())
 	{
+		if (_state == Title)
+		{
+			_isActive = false;
+			_timer = 0;
+			_isActiveTimer = 0;
+
+			_text(U"Click to MouseLeftButton").drawAt(Scene::Center(), Palette::Skyblue);
+			if (MouseL.down())
+			{
+				_state = InGame;
+			}
+		}
+
+		if (_state == Finish)
+		{
+			_text(U"Finish").drawAt(Scene::Center(), Palette::Skyblue);
+
+			if (MouseL.down())
+			{
+				ball.setPos(400, 400);
+				ballVec.x = 0;
+				for (int i = 0; i < X_COUNT; ++i)
+				{
+					for (int j = 0; j < Y_COUNT; ++j)
+					{
+						int index = (j * X_COUNT) + i;
+
+						bricks[index] = Rect{i * BRICK_SIZE.x, 60 + j * BRICK_SIZE.y, BRICK_SIZE};
+					}
+				}
+
+				_state = Title;
+			}
+		}
+
 #pragma region 更新処理
 
-		const Rect paddle{ Arg::center(Cursor::Pos().x, 500), 60, 10 };
+		const Rect paddle{Arg::center(Cursor::Pos().x, 500), 60, 10};
 
-		ball.moveBy(ballVec * Scene::DeltaTime());
+		const Rect npcPaddle{Arg::center(Cursor::Pos().x, 250), 60, 10};
+
+		if (_state == InGame)
+		{
+			ball.moveBy(ballVec * Scene::DeltaTime());
+			_timer += Scene::DeltaTime();
+
+			if (!_isActive && _timer >= 5)
+			{
+				_isActive = true;
+				_isActiveTimer = 0;
+			}
+
+			if (_isActive)
+			{
+				_isActiveTimer += Scene::DeltaTime();
+
+				npcPaddle.rounded(3).draw();
+				// NPCパドルとの衝突
+				if ((0 < ballVec.y) && ball.y < npcPaddle.y && npcPaddle.intersects(ball))
+				{
+					ballVec = Vec2{(ball.x - npcPaddle.center().x) * 10, -ballVec.y}.setLength(BALL_SPEED * 1.7);
+				}
+				if ((0 > ballVec.y) && ball.y > npcPaddle.y && npcPaddle.intersects(ball))
+				{
+					ballVec = Vec2{(ball.x - npcPaddle.center().x) * 10, -ballVec.y}.setLength(BALL_SPEED * 1.7);
+				}
+
+				if (_isActiveTimer >= 8)
+				{
+					_isActive = false;
+					_timer = 0;
+				}
+			}
+		}
 
 #pragma endregion
 
@@ -72,6 +160,12 @@ void Main()
 			ballVec.y *= -1;
 		}
 
+		// ボールが下に落ちた時
+		if (ball.y > Scene::Height())
+		{
+			_state = Finish;
+		}
+
 		// 壁との衝突
 		if (((ball.x < 0) && ballVec.x < 0) || ((Scene::Width() < ball.x) && (0 < ballVec.x)))
 		{
@@ -81,7 +175,7 @@ void Main()
 		// パドルとの衝突
 		if ((0 < ballVec.y) && paddle.intersects(ball))
 		{
-			ballVec = Vec2{ (ball.x - paddle.center().x) * 10, -ballVec.y }.setLength(BALL_SPEED);
+			ballVec = Vec2{(ball.x - paddle.center().x) * 10, -ballVec.y}.setLength(BALL_SPEED);
 		}
 
 #pragma endregion
@@ -90,12 +184,11 @@ void Main()
 #pragma region 描画処理
 
 		ball.draw();
-
 		paddle.rounded(3).draw();
 
 		for (int i = 0; i < MAX; ++i)
 		{
-			bricks[i].stretched(-1).draw(HSV{bricks[i].y -40});
+			bricks[i].stretched(-1).draw(HSV{bricks[i].y - 40});
 		}
 
 #pragma endregion
